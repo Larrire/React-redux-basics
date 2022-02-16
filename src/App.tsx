22d9705f4aa304cfc08a78b687f69901c90d0bec
Larrire <linekerlarrire@gmail.com>
2022-02-16 06:38:59 -0300

    Getting data from redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css';
+import { useSelector } from 'react-redux';
+import { RootState } from './redux/store';
 
 function App() {
+
+  const user = useSelector((state: RootState) => state.user);
+
   return (
     <>
-      <h1>Ola, meu nome é ... e tenho ... anos.</h1>
+      <h1>Ola, meu nome é {user.name} e tenho {user.age} anos.</h1>
       <h2>Tema ...</h2>
 
       <hr />
 
-      <input type="text" value={'...'} />
+      <input type="text" value={user.name} />
 
       <hr />
 
