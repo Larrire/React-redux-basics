0e3070310fb9d8f55ceff33105b507e244d87879
Larrire <linekerlarrire@gmail.com>
2022-02-16 07:12:26 -0300

    Using data from themeReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useReduxSelector } from './redux/hooks/useReduxSelector'
 import { setName, setAge } from './redux/reducers/userReducer';
+import { setThemeStatus } from './redux/reducers/themeReducer';
+import './App.css'
 
 function App() {
   const dispatch = useDispatch();
 
   const user = useReduxSelector(state => state.user);
+  const theme = useReduxSelector(state => state.theme);
 
   const changeName = (newName: string) => dispatch( setName(newName) );
   const changeAge = (newAge: number) => dispatch( setAge(newAge) );
+  const switchTheme = (newTheme: string) => dispatch( setThemeStatus(newTheme) );
 
   const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     changeName(e.target.value);
@@ -19,10 +23,14 @@ function App() {
     changeAge(parseInt(e.target.value));
   }
 
+  const handleSwitchTheme = () => {
+    switchTheme(theme.status === 'light' ? 'dark' : 'light')
+  }
+
   return (
-    <>
+    <main className={ theme.status === 'dark' ? 'dark-theme' : '' }>
       <h1>Ola, meu nome é {user.name} e tenho {user.age} anos.</h1>
-      <h2>Tema ...</h2>
+      <h2>Tema: {theme.status}</h2>
 
       <hr />
 
@@ -31,8 +39,8 @@ function App() {
 
       <hr />
 
-      <button>Switch theme</button>
-    </>
+      <button onClick={handleSwitchTheme}>Switch theme</button>
+    </main>
   );
 }
 
